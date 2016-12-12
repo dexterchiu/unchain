249ab40a721a95dd36ecf58d04f3eff8d6aa9607
Dexter Chiu <dexter@cryptoentertainment.com>
2016-12-11 17:16:27 -0800

    Initial consideration of view. To use ListView (should think of something else)

--- unchain/queue/views.py
+++ unchain/queue/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from django.views.generic import ListView
+from django.views.generic.list import MultipleObjectMixin
+
+
 # Create your views here.
+
+class QueueView(MultipleObjectMixin, ListView):
+    pass
