17778b4da7823d10167c045cd4b28dbfdb13bbdc
Dexter Chiu <dexter@cryptoentertainment.com>
2017-03-20 21:20:06 -0700

    Fix adding context names and template links to the list views

--- unchain/queue/views.py
+++ unchain/queue/views.py
@@ -7,7 +7,11 @@ from .models import Party, Table
 
 class PartyListView(ListView):
     model = Party
+    template_name = 'queue/party_list.html'
+    context_object_name = 'party_list'
 
 
 class TableListView(ListView):
     model = Table
+    template_name = 'queue/table_list.html'
+    context_object_name = 'table_list'
