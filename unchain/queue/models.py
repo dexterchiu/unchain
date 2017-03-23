8e4563227e30346eb553c747dd4449344ba3688a
Dexter Chiu <dexter@cryptoentertainment.com>
2017-03-22 21:29:53 -0700

    Switch to using one-to-one relation, and ensured stability for list view

--- unchain/queue/models.py
+++ unchain/queue/models.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 from django.utils import timezone
+from django.core.exceptions import ObjectDoesNotExist
 
 
 # Create your models here.
@@ -16,22 +17,33 @@ class Table(models.Model):
         (EIGHT_SEATER, 'eight seater'),
     )
     capacity = models.IntegerField(choices=CAPACITY_CHOICES, default=TWO_SEATER)
-    # table number for easy identification
-    number = models.IntegerField()
     # use quotes to force a lazy reference
     # might want to consider switching to a one-to-one relation instead
-    occupant = models.ForeignKey('Party', null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
-        return str("Table of capacity {}, occupant: {}".format(self.capacity,
-                                                               self.occupant))
+        try:
+            return str("Table {} of capacity {}, occupant: {}".format(
+                self.id,
+                self.capacity,
+                self.party,
+            ))
+        except ObjectDoesNotExist:
+            return str("Table {} of capacity {}, unoccupied".format(
+                self.id,
+                self.capacity,
+            ))
 
 
 class Party(models.Model):
     name = models.CharField(max_length=200)
     size = models.IntegerField()
     arrival_time = models.DateTimeField(editable=False)
-    seat = models.ForeignKey(Table, null=True, on_delete=models.SET_NULL)
+    seat = models.OneToOneField(
+        Table,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+    )
 
     def save(self, *args, **kwargs):
         '''overloading save to ensure arrival time is always now'''
@@ -40,6 +52,9 @@ class Party(models.Model):
         return super(Party, self).save(*args, **kwargs)
 
     def __str__(self):
-        return str("{}'s party of {}, arrived @ {}".format(self.size,
-                                                           self.name,
+        return str("{}'s party of {}, arrived @ {}".format(self.name,
+                                                           self.size,
                                                            self.arrival_time))
+
+    class Meta:
+        verbose_name_plural = 'parties'
